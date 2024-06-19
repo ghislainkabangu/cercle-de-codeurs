16cedfc6c0f57eca7ef4a29a02f8b24753b19ee7
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-06-19 12:57:33 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,38 @@
+/** @format */
+
+let main_contenaire_Div1 = document.getElementById("main_contenaire_Div1");
+let imageService1 = document.getElementById("imageService1");
+main_contenaire_Div1.addEventListener("mouseover", () => {
+  imageService1.src = "album/james-harrison-vpOeXr5wmR4-unsplash.jpg";
+});
+main_contenaire_Div1.addEventListener("mouseout", () => {
+  imageService1.src = "album/nubelson-fernandes-UcYBL5V0xWQ-unsplash.jpg";
+});
+/**************************************************** */
+let main_contenaire_Div2 = document.getElementById("main_contenaire_Div2");
+let imageService2 = document.getElementById("imageService2");
+main_contenaire_Div2.addEventListener("mouseover", () => {
+  imageService2.src = "album/fahim-muntashir-v-FOvoL3onk-unsplash.jpg";
+});
+main_contenaire_Div2.addEventListener("mouseout", () => {
+  imageService2.src = "album/altumcode-PuxTq4DwUHE-unsplash.jpg";
+});
+/********************************************************* */
+let main_contenaire_Div3 = document.getElementById("main_contenaire_Div3");
+let imageService3 = document.getElementById("imageService3");
+main_contenaire_Div3.addEventListener("mouseover", () => {
+  imageService3.src = "album/ux-indonesia-qC2n6RQU4Vw-unsplash.jpg";
+});
+main_contenaire_Div3.addEventListener("mouseout", () => {
+  imageService3.src = "album/hal-gatewood-tZc3vjPCk-Q-unsplash.jpg";
+});
+/********************************************************** */
+let main_contenaire_Div4 = document.getElementById("main_contenaire_Div4");
+let imageService4 = document.getElementById("imageService4");
+main_contenaire_Div4.addEventListener("mouseover", () => {
+  imageService4.src = "album/marvin-meyer-SYTO3xs06fU-unsplash.jpg";
+});
+main_contenaire_Div4.addEventListener("mouseout", () => {
+  imageService4.src = "album/scott-graham-5fNmWej4tAA-unsplash.jpg";
+});
+/*___________________________________________________________________________________*/
