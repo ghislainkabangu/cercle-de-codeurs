b2607a1804cd728c8715ba134d5758b035072c2f
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-10-15 19:12:42 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,9 @@
 /** @format */
+/*
 let baniere = document.querySelector(".banniere");
 baniere.addEventListener("mouseover", () => {
   baniere.classList.add("JsimageBg");
 });
 baniere.addEventListener("mouseout", () => {
   baniere.classList.remove("JsimageBg");
-});
+});*/
